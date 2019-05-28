6de53209df19db79a7f005d50950aeb8550950ec
Amos Gheva <ghevaamos@gmail.com>
2019-05-28 16:10:12 -0400

    Adding macros to allow for added files be linked into executables, this is usefule for unittests. Added a mecro to assist with gtest linking etc

--- functions.mk
+++ functions.mk
@@ -126,9 +126,9 @@ define MESSAGE
 endef
 
 define EXE_TARGET
-$(call EXE_NAME, ${1}): ${$(strip ${1})_DEPS} $(call STATC_LIB_EXE_DEPS, ${1})
+$(call EXE_NAME, ${1}): ${$(strip ${1})_DEPS} $(call STATC_LIB_EXE_DEPS, ${1}) ${$(strip ${1})_EXTRAS}
 	$(call MESSAGE, Linking executable $$@)
-	${QQ}${CXX} ${CXXFLAGS} ${CFLAGS} ${LDFLAGS} -o $$@ ${$(strip ${1})_LINK_OBJS} $(call LINK_LIBS, ${$(strip ${1})_MODULES}) ${LDFLAGS}
+	${QQ}${CXX} ${CXXFLAGS} ${CFLAGS} ${LDFLAGS} -o $$@ ${$(strip ${1})_LINK_OBJS} ${$(strip ${1})_EXTRAS} $(call LINK_LIBS, ${$(strip ${1})_MODULES}) ${LDFLAGS}
 endef
 
 define ADD_EXE
@@ -140,6 +140,10 @@ DIR_SUBDIRS :=
 DIR_EXPORT_HEADERS :=
 DIR_OBJS :=
 DIR_EXES :=
+endef
+
+define ADD_UNITESTS
+$(strip ${1})_EXTRAS += $(call OBJ_NAME, $(strip ${2})/unittests, ${3})
 endef
 
 # vim: set noexpandtab :
